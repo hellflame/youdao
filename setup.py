7246ebbe641614f8daddce2f1043f53c8e5696e9
hellflame <1138465446@qq.com>
2015-09-07 15:23:01 +0800

    adjust

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ __author__ = 'hellflame'
 from setuptools import setup, find_packages
 
 setup(
-    name='Youdao',
-    version="1.0",
+    name='YoudaoDict',
+    version="1.1",
     keywords=('Youdao', 'youdao', 'dict', 'youdao api'),
     description="simply deploy youdao web api , translate the words you don't know, specially for console users",
     license='MIT License',
@@ -15,7 +15,7 @@ setup(
     platforms="linux, mac os",
     entry_points={
         'console_scripts': [
-            'youdao_test=youdao.youdao:main'
+            'youdao=youdao.youdao:main'
         ]
     }
 )
