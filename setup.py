41baec2901581f2f37e009eb876702fd7ae0cf0d
hellflame <1138465446@qq.com>
2015-09-13 13:29:30 +0800

    修改版本号,以及发布简介

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
+# coding=utf8
 __author__ = 'hellflame'
 
 from setuptools import setup, find_packages
 
 setup(
     name='YoudaoDict',
-    version="1.1.1",
+    version="1.1.2",
     keywords=('Youdao', 'youdao', 'dict', 'youdao api'),
-    description="simply deploy youdao web api , translate the words you don't know, specially for console users",
+    description="适用于在linux or mac 终端通过有道api查询单词或者翻译词句",
     license='MIT License',
     author='hellflame',
     author_email='hellflamedly@gmail.com',
