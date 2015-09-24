227fb8aff3c216d9ff1549c2e2d25a87070f03e4
hellflame <1138465446@qq.com>
2015-09-24 18:57:33 +0800

    setup信息完善

--- setup.py
+++ setup.py
@@ -5,15 +5,21 @@ from setuptools import setup, find_packages
 
 setup(
     name='YoudaoDict',
-    version="1.1.5",
+    version="1.1.5.1",
     keywords=('Youdao', 'youdao', 'dict', 'youdao api'),
     description="适用于在linux or mac 终端通过有道api查询单词或者翻译词句",
-    license='MIT License',
+    license='MIT',
     author='hellflame',
     author_email='hellflamedly@gmail.com',
     url="https://github.com/hellflame/youdao",
     packages=find_packages(),
     platforms="linux, mac os",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7'
+    ],
     entry_points={
         'console_scripts': [
             'youdao=youdao.youdao:main'
